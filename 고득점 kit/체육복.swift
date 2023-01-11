9081bf8525ef4d9b2a618551a7242dbf6f226591
B0X <83914919+BOLTB0X@users.noreply.github.com>
2023-01-12 00:07:15 +0900

    commit

---
+++ 고득점 kit/체육복.swift
@@ -0,0 +1,40 @@
+import Foundation
+
+func solution(_ n:Int, _ lost:[Int], _ reserve:[Int]) -> Int {
+    var answer = 0
+    // 학생들의 체육복 배열
+    var students = [Int] (repeating: 1, count: n + 1)
+    
+    // 잃어버린 학생
+    for l in lost {
+        students[l] -= 1
+    }
+    
+    // 여분
+    for r in reserve {
+        students[r] += 1
+    }
+    
+    for i in 1..<n+1 {
+        if students[i] == 0 {
+            // 앞에 학생에게 빌리는 경우
+            if i > 1 && students[i - 1] > 1 {
+                students[i - 1] -= 1
+                students[i] = 1
+            }
+        
+            // 뒤 애한테 빌리는 경우
+            else if i < n && students[i + 1] > 1 {
+                students[i + 1] -= 1
+                students[i] = 1
+            }
+        }
+    }
+    
+    for st in students {
+        if st >= 1 {
+            answer += 1
+        }
+    }
+    return answer - 1 // 배열의 편의를 위해 인덱스 + 1 했기 때문
+}
