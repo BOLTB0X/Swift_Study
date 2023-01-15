562db250f806735fc5fd2e22a8df1e5f93054c1b
BOLTB0X <khunlee0508@gmail.com>
2023-01-15 22:53:18 +0900

    commit

--- 코딩테스트입문/숨어있는 숫자의 덧셈 (1).swift
+++
@@ -1,5 +0,0 @@
-import Foundation
-
-func solution(_ my_string:String) -> Int {
-    return my_string.compactMap { Int(String($0)) }.reduce(0) { $0 + $1 }
-}
--- 코딩테스트입문/피자 나눠 먹기 (2).swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-func solution(_ n:Int) -> Int {
-    for i in 1...101 where i * 6 % n == 0 {
-            return i
-    }
-    return 0
-}
