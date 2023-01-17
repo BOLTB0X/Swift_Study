c35c6dcea1c4179d6cd8f6b395560059acf700ff
BOLTB0X <khunlee0508@gmail.com>
2023-01-17 23:13:36 +0900

    commit

--- Playground/00_03_datatype.playground/Contents.swift
+++ Playground/00_03_datatype.playground/Contents.swift
@@ -5,4 +5,34 @@ someBool = false
 // someBool = 0 // 컴파일 오류발생
 // someBool = 1 // 컴파일 오류발생
 
-print(someBool)
+// Float
+var someFloat: Float = 3.14
+someFloat = 3
+
+// Double
+var someDouble: Double = 3.14
+// someDouble = 3
+// someDouble = someFloat // 컴파일 오류발생
+
+// Character
+var someCharacter: Character = "🇰🇷"
+someCharacter = "😄"
+someCharacter = "가"
+someCharacter = "A"
+// someCharacter = "하하하" // 컴파일 오류발생
+print(someCharacter)
+
+// String
+var someString: String = "하하하 😄 "
+someString = someString + "웃으면 복이와요"
+//print(someString)
+// someString = someCharacter // 컴파일 오류발생
+
+// 생각해보기
+let integer = 100
+let floatingPoint = 12.34
+let apple = "A"
+
+print(type(of:integer))
+print(type(of:floatingPoint))
+print(type(of:apple))
