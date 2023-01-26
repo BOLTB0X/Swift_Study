9e24f5563c5d4cffbfc919544b45821fcd4959b1
BOLTB0X <khunlee0508@gmail.com>
2023-01-26 21:05:43 +0900

    commit

--- 코딩테스트입문/제곱수 판별하기.swift
+++ 코딩테스트입문/제곱수 판별하기.swift
@@ -1,14 +1,8 @@
 import Foundation
 
 func solution(_ n:Int) -> Int {
-    for i in 1...1000000 {
-        if i * i == n {
-            return 1
-        }
-        // 여기가 왔다면
-        else if i == 1000000 {
-            return 2
-        }
-    }
-    return 0
+    // 다른 풀이
+    // floor = 소수점을 버리는 함수
+    // 암시적 표현
+    sqrt(Double(n)) == floor(sqrt(Double(n))) ? 1 : 2
 }
