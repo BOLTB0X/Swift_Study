a45552c9c4f754d6a79cce51ea28bedd37b96dd5
BOLTB0X <khunlee0508@gmail.com>
2023-02-19 14:57:18 +0900

    commit

--- 고득점 kit/소수 찾기.swift
+++ 고득점 kit/소수 찾기.swift
@@ -1,49 +1,51 @@
 import Foundation
 
-// 정답
-var answer: Set<Int> = []
+func solution(_ numbers:String) -> Int {
+    var answer: Set<Int> = [] // 정답 집합 중복 제거용
+    var numbersArr = numbers.map{String($0)} // 편의를 위해
 
-func isPrime(num: String) -> Bool {
-    let transNum = Int(num)! // 변환
-    
-    if transNum < 4 { // 4이하일 경우
-        return transNum == 1 ? false : true
-    }
+    func isPrime(_ num: String) {
+    let num = Int(num)!
+    var cnt = 0
 
-    for i in 2...Int(sqrt(Double(transNum))) {
-        if transNum % i == 0 {
-            return false
+    if num > 1{
+        for i in 2...num{
+            if cnt > 1{
+                break
+            }
+            if num % i == 0 {
+                cnt += 1
+            }
+        }
+
+        if cnt == 1{
+            // 3. 소수이면 Set에 추가합니다.
+            answer.insert(num) // Set에 넣어주어 중복 제거
         }
     }
-
-    return true
 }
+    
+    // DFS로 순열 구현
+    func DFS(array: [String], number: String, n: Int, level: Int) {
+        // 탈출 조건
+        if level == n {
+            isPrime(number) // 알아서 중복 제거
+            return
+        }
 
-// DFS로 순열 구현
-func DFS(array: [String], number: String, n: Int, level: Int) {
-    // 탈출 조건
-    if level == n && isPrime(num: number) == true {
-        answer.append(Int(number)!) // 알아서 중복 제거
+        // 배열 순회
+        for i in 0..<array.count {
+            var new_array = array // var로 교체
+            let str = new_array.remove(at: i) // 제거와 제거한 무자
+            DFS(array: new_array, number: "\(number)\(String(str))", n: n, level: level + 1)
+        }
+        
         return
     }
-
-    // 배열 순회
-    for i in 0..<array.count {
-        var new_array = array // var로 교체
-        let str = new_array.remove(at: i) // 제거와 제거한 무자
-        DFS(array: new_array, number: "\(number)\(String(str))", n: n, level: level + 1)
+    
+    // 1부터 문자열의 길이까지 실행
+    for i in 1...numbers.count {
+        DFS(array: numbersArr, number: "", n: i, level: 0)
     }
-}
-
-func solution(_ numbers:String) -> Int {
-    let size = numbers.count
-    let arr = numbers.map{String($0)} // 배열로 복사
-
-    // 1부터 배열의 길이만큼
-    // 돌려서 자리수에 해당하는 소수를 찾음
-    for i in 1...size{
-        DFS(array: arr, number: "", n: i, level: 0)
-    }
-
-    return answer.count
+    return answer.count // 길이가 곹 갯수
 }
