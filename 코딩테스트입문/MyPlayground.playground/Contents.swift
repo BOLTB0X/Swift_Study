617700ee8fbd70c9ad4fdf403a05f2ba4b3910b1
BOLTB0X <khunlee0508@gmail.com>
2023-03-04 14:32:53 +0900

    commit

--- 코딩테스트입문/MyPlayground.playground/Contents.swift
+++ 코딩테스트입문/MyPlayground.playground/Contents.swift
@@ -1,30 +1,37 @@
 import UIKit
-var coke = 20
 
-var array = [3, 10, 28]
-var n = 20
+var N:Int = 5
+var stages: [Int] = [2, 1, 2, 6, 2, 4, 3, 3]
 
-var answer : [Int] = []
-var p = [93,30,55]
-var speeds = [1,30,5]
-
-print(speeds.sorted(by: >))
-
-while p.count > 0 {
-    var cnt: Int = 0
-    for i in 0..<speeds.count {
-        p[i] += speeds[i] // 진도
+// 인덱스-> 스테이지, 값 진행자 수
+    var stageFail = Array(repeating: 0, count: N+2)
+    var stageSuccess = Array(repeating: 0, count: N+2)
+    
+    // 진행자들 카운팅
+    for i in 0..<stages.count {
+        stageFail[stages[i]] += 1
     }
     
-    while p.count > 0 && p[0] >= 100 {
-        p.removeFirst()
-        speeds.removeFirst()
-        cnt += 1
+    var tmp:Int = stages.count // 총 인원
+    for i in 1..<N+2 {
+        stageSuccess[i] = tmp - stageFail[i]
+        tmp -= stageFail[i]
     }
-    
-    if cnt > 0 {
-        answer.append(cnt)
-    }
+
+print(stageFail)
+print(stageSuccess)
+
+var failPercents: Dictionary = [Int: Double]()
+
+for i in 1..<N+1 {
+    let failPer = Double(stageFail[i]) / Double(stageSuccess[i])
+    failPercents[i] = failPer
 }
 
+// 정렬
+let tmpfailPercents = failPercents.sorted(by: <).sorted(by: {$0.1 > $1.1})
+
+print(failPercents)
+
+var answer = tmpfailPercents.map {$0.key}
 print(answer)
